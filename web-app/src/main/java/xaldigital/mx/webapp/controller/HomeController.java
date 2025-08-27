5b2d4b14dc3ee6eb9cadc2c9c021fe7b2a4ca303
Eduardo Elliot Martinez Palomino <elliotmtz12@gmail.com>
2025-08-26 17:21:47 -0700

    configured java spring boot project & added consumption of api

--- web-app/src/main/java/xaldigital/mx/webapp/controller/HomeController.java
+++ web-app/src/main/java/xaldigital/mx/webapp/controller/HomeController.java
@@ -1,17 +1,33 @@
 package xaldigital.mx.webapp.controller;
 
+import java.io.FileNotFoundException;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import jakarta.servlet.http.HttpServletRequest;
+import xaldigital.mx.webapp.service.ServiceLocator;
+import xaldigital.mx.webapp.service.entity.MyCV;
 
 
 @Controller
 public class HomeController {
     
+    @Autowired
+    private ServiceLocator serviceLocator;
+
     @GetMapping("/home")
     public String home(Model model, HttpServletRequest request) {        
         return "/home";
     }    
+
+    @GetMapping("/mycv")
+    public String mycv(Model model, HttpServletRequest request) throws FileNotFoundException  {        
+        MyCV mycv = serviceLocator.getInstanceXalDigitalDao().getMyCV(request);
+
+        model.addAttribute("mycv", mycv);
+        return "/mycv";
+    }        
 }
--- web-app/src/main/java/xaldigital/mx/webapp/service/entity/MyCV.java
+++ web-app/src/main/java/xaldigital/mx/webapp/service/entity/MyCV.java
@@ -1,137 +1,100 @@
 package xaldigital.mx.webapp.service.entity;
 
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
 import javax.annotation.Generated;
-import com.fasterxml.jackson.annotation.JsonAnyGetter;
-import com.fasterxml.jackson.annotation.JsonAnySetter;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
 
-@JsonInclude(JsonInclude.Include.NON_NULL)
-@JsonPropertyOrder({
-        "academic_history",
-        "profile_img",
-        "portfolio",
-        "CVID",
-        "about_me",
-        "languages",
-        "external_links",
-        "music_projects"
-})
 @Generated("jsonschema2pojo")
 public class MyCV {
 
-    @JsonProperty("academic_history")
+    @SerializedName("academic_history")
+    @Expose
     private List<AcademicHistory> academicHistory;
-    @JsonProperty("profile_img")
+    @SerializedName("profile_img")
+    @Expose
     private String profileImg;
-    @JsonProperty("portfolio")
+    @SerializedName("portfolio")
+    @Expose
     private List<Portfolio> portfolio;
-    @JsonProperty("CVID")
+    @SerializedName("CVID")
+    @Expose
     private String cvid;
-    @JsonProperty("about_me")
+    @SerializedName("about_me")
+    @Expose
     private String aboutMe;
-    @JsonProperty("languages")
+    @SerializedName("languages")
+    @Expose
     private List<Language> languages;
-    @JsonProperty("external_links")
+    @SerializedName("external_links")
+    @Expose
     private ExternalLinks externalLinks;
-    @JsonProperty("music_projects")
+    @SerializedName("music_projects")
+    @Expose
     private List<MusicProject> musicProjects;
-    @JsonIgnore
-    private Map<String, Object> additionalProperties = new LinkedHashMap<String, Object>();
 
-    @JsonProperty("academic_history")
     public List<AcademicHistory> getAcademicHistory() {
         return academicHistory;
     }
 
-    @JsonProperty("academic_history")
     public void setAcademicHistory(List<AcademicHistory> academicHistory) {
         this.academicHistory = academicHistory;
     }
 
-    @JsonProperty("profile_img")
     public String getProfileImg() {
         return profileImg;
     }
 
-    @JsonProperty("profile_img")
     public void setProfileImg(String profileImg) {
         this.profileImg = profileImg;
     }
 
-    @JsonProperty("portfolio")
     public List<Portfolio> getPortfolio() {
         return portfolio;
     }
 
-    @JsonProperty("portfolio")
     public void setPortfolio(List<Portfolio> portfolio) {
         this.portfolio = portfolio;
     }
 
-    @JsonProperty("CVID")
     public String getCvid() {
         return cvid;
     }
 
-    @JsonProperty("CVID")
     public void setCvid(String cvid) {
         this.cvid = cvid;
     }
 
-    @JsonProperty("about_me")
     public String getAboutMe() {
         return aboutMe;
     }
 
-    @JsonProperty("about_me")
     public void setAboutMe(String aboutMe) {
         this.aboutMe = aboutMe;
     }
 
-    @JsonProperty("languages")
     public List<Language> getLanguages() {
         return languages;
     }
 
-    @JsonProperty("languages")
     public void setLanguages(List<Language> languages) {
         this.languages = languages;
     }
 
-    @JsonProperty("external_links")
     public ExternalLinks getExternalLinks() {
         return externalLinks;
     }
 
-    @JsonProperty("external_links")
     public void setExternalLinks(ExternalLinks externalLinks) {
         this.externalLinks = externalLinks;
     }
 
-    @JsonProperty("music_projects")
     public List<MusicProject> getMusicProjects() {
         return musicProjects;
     }
 
-    @JsonProperty("music_projects")
     public void setMusicProjects(List<MusicProject> musicProjects) {
         this.musicProjects = musicProjects;
-    }
-
-    @JsonAnyGetter
-    public Map<String, Object> getAdditionalProperties() {
-        return this.additionalProperties;
-    }
-
-    @JsonAnySetter
-    public void setAdditionalProperty(String name, Object value) {
-        this.additionalProperties.put(name, value);
     }
 
 }
