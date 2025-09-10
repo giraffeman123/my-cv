c8dfcfa2cbb288254959dfaef7a5e5e29f64bb47
Eduardo Elliot Martinez Palomino <elliotmtz12@gmail.com>
2025-09-10 11:15:16 -0700

    troubleshooting appspec file

--- scripts/deploy_webapp.sh
+++ scripts/deploy_webapp.sh
@@ -2,27 +2,28 @@
 
 date_of_deploy=$(date +"%Y-%m-%d_%H-%M-%S")
 
-sudo touch "webappdeploy$date_of_deploy.log"
+# sudo touch "webappdeploy$date_of_deploy.log"
 
+echo "---------------------------------Stopping Tomcat---------------------------------"
 sudo systemctl stop tomcat
 
-echo "---------------------------------Start Of Install Script: $date_of_deploy---------------------------------" | sudo tee -a "webappdeploy$date_of_deploy.log"
+# echo "---------------------------------Start Of Install Script: $date_of_deploy---------------------------------"
 
-# Instalacion paquetes necesarios
-sudo apt-get update
+# # Instalacion paquetes necesarios
+# sudo apt-get update
 
-# enter into git repository of project and fetch latests changes
-cd /home/ubuntu/my-cv-latest/
+# # enter into git repository of project and fetch latests changes
+# cd /home/ubuntu/my-cv-latest/
 
-# enter into web-app, build .war file and update tomcat
-cd web-app/
-echo "---------------------------------Building .WAR---------------------------------" | sudo tee -a "webappdeploy$date_of_deploy.log"
-sudo mvn clean package 
-sudo chown -R ubuntu:ubuntu target/WebApp.war
-echo "---------------------------------Replacing old .WAR with latest---------------------------------" | sudo tee -a "webappdeploy$date_of_deploy.log"
-sudo mv /home/ubuntu/my-cv-latest/web-app/target/WebApp.war /opt/tomcat/webapps/ROOT.war
-sudo rm -rf /opt/tomcat/webapps/ROOT/
+# # enter into web-app, build .war file and update tomcat
+# cd web-app/
+# echo "---------------------------------Building .WAR---------------------------------"
+# sudo mvn clean package 
+# sudo chown -R ubuntu:ubuntu target/WebApp.war
+# echo "---------------------------------Replacing old .WAR with latest---------------------------------"
+# sudo mv /home/ubuntu/my-cv-latest/web-app/target/WebApp.war /opt/tomcat/webapps/ROOT.war
+# sudo rm -rf /opt/tomcat/webapps/ROOT/
 
-echo "---------------------------------Restarting Tomcat---------------------------------" | sudo tee -a "webappdeploy$date_of_deploy.log"
-sudo systemctl restart tomcat
+# echo "---------------------------------Restarting Tomcat---------------------------------"
+# sudo systemctl restart tomcat
 
